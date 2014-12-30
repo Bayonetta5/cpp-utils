364e4e33dc225c2958d2f6a58390bdf222324a61
Krozark <maxime.barbier1991@gmail.com>
2014-12-30 13:47:38 +0100

    add error msg

--- examples/json-test.cpp
+++ examples/json-test.cpp
@@ -2,6 +2,12 @@
 
 int main(int argc,char* argv[])
 {
+    if(argc <2)
+    {
+        std::cerr<<"A json file is required as parameter"<<std::endl;
+        return 0;
+    }
+        
     utils::json::Value* v = utils::json::Driver::parse_file(argv[1]);
     std::cout<<*v<<std::endl;
 
